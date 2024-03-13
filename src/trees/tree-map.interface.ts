3eb86fefa37b9fdcb2b5163639aa8f8ff4396c44
Denis Morosenko <damoroze@mts.ru>
2024-03-13 17:16:05 +0300

    Fix interface

--- src/trees/tree-map.interface.ts
+++ src/trees/tree-map.interface.ts
@@ -2,7 +2,12 @@ import { TreeMapIterator } from './tree-map-iterator.interface';
 import { TreeMapConvertor } from './tree-map-convertor.interface';
 import { TreeSet } from './tree-set.interface';
 
-export interface TreeMap<K, V> extends Omit<TreeSet<K>, 'add'>, TreeMapIterator<K, V>, TreeMapConvertor<V> {
+type BaseTree<K> = Omit<
+  Omit<TreeSet<K>, 'add'>,
+  'toArray'
+>;
+
+export interface TreeMap<K, V> extends BaseTree<K>, TreeMapIterator<K, V>, TreeMapConvertor<V> {
   get(key: K): V | undefined;
 
   add(key: K, value: V): number;
