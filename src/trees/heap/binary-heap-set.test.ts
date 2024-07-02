77f2032cc24e84d56ccd1787187678ab62b14439
Oppositus <dem@devilstar.ru>
2024-07-02 19:19:00 +0300

    Binary heap (no tests)

--- src/trees/heap/binary-heap-set.test.ts
+++ src/trees/heap/binary-heap-set.test.ts
@@ -1,4 +1,4 @@
-import { BinaryMaxHeapSet } from './binary-heap-set';
+import { BinaryMaxHeapSet, BinaryMinHeapSet } from './binary-heap-set';
 
 function isMaxHeap<K>(heap: K[], index: number = 0): boolean {
   if (index >= heap.length) {
@@ -26,7 +26,7 @@ function isMaxHeap<K>(heap: K[], index: number = 0): boolean {
   }
 
   if (!isChild1Ok || !isChild2Ok) {
-    // console.log('*** FAIL', heap[index], heap[child1], heap[child2]);
+    console.log('*** FAIL', `[${index}]`, heap[index], `[${child1}]`, heap[child1], `[${child2}]`, heap[child2]);
     return false;
   }
 
@@ -63,7 +63,130 @@ function isMinHeap<K>(heap: K[], index: number = 0): boolean {
     return false;
   }
 
-  return isMaxHeap(heap, child1) && isMaxHeap(heap, child2);
+  return isMinHeap(heap, child1) && isMinHeap(heap, child2);
+}
+
+function mergeHeaps(a: number[], b: number[]): number[] {
+  const res: number[] = [];
+
+  const n = a.length;
+  const m = b.length;
+
+  // vis_next1 & vis_next2 tells us next element of array "a"
+  // and array "b" is visited or not respectively.
+  let vis_next1 = false;
+  let vis_next2 = false;
+  // i & j is current index of array "a" and "b" respectively
+  let i = 0;
+  let j = 0;
+  
+  // Loop until end of array "a" or "b"
+  while (i !== n && j !== m) {
+      let max1 = i;
+      let max2 = j;
+
+      // Check if next element of array "a" is greater than 
+      // current element
+      if (i + 1 !== n && !vis_next1 && a[i + 1] > a[i])
+          max1 = i + 1;
+
+      // Check if next element of array "b" is greater than 
+      // current element
+      if (j + 1 !== m && !vis_next2 && b[j + 1] > b[j])
+          max2 = j + 1;
+
+      // Compare the maximum elements from both arrays
+      if (a[max1] > b[max2]) {
+          // console.log(a[max1] + " ");
+          res.push(a[max1]);
+
+          // Update index and vis_next1 for array "a"
+          if (max1 === i + 1)
+              vis_next1 = true;
+          else {
+              if (vis_next1) {
+                  i += 2;
+                  vis_next1 = false;
+              }
+              else
+                  i++;
+          }
+      }
+      else {
+          // console.log(b[max2] + " ");
+          res.push(b[max2]);
+
+          // Update index and vis_next2 for array "b"
+          if (max2 === j + 1)
+              vis_next2 = true;
+          else {
+              if (vis_next2) {
+                  j += 2;
+                  vis_next2 = false;
+              }
+              else
+                  j++;
+          }
+      }
+  }
+
+  // Print remaining elements of array "b" if array "a" is
+  // exhausted
+  if (i === n) {
+      while (j !== m) {
+          // console.log(b[j] + " ");
+          res.push(b[j]);
+          j++;
+          if (vis_next2) {
+              vis_next2 = false;
+              j++;
+          }
+      }
+  }
+  // Print remaining elements of array "a" if array "b" is
+  // exhausted
+  else {
+      while (i !== n) {
+          // console.log(a[i] + " ");
+          res.push(a[i]);
+          i++;
+          if (vis_next1) {
+              vis_next1 = false;
+              i++;
+          }
+      }
+  }
+
+  return res;
+}
+
+function mergeHeaps2(a: number[], b: number[]): number[] {
+  const ans: number[] = [];
+  let i = 0;
+  let j=0;
+
+  while(i < a.length && j < b.length){
+      if(a[i] > b[j]){
+          ans.push(a[i++]);
+      } else {
+          ans.push(b[j++]);
+      }
+  }
+  while(i < a.length) {
+    ans.push(a[i++]);
+  }
+
+  while(j < b.length) {
+    ans.push(b[j++]);
+  }
+  
+  return ans;
+}
+
+function mergeHeaps3(a: number[], b: number[]): number[] {
+  const res = a.concat(b);
+  const h = BinaryMaxHeapSet.heapify(res);
+  return h['heap'];
 }
 
 describe('MaxHeap', () => {
@@ -85,5 +208,150 @@ describe('MaxHeap', () => {
       expect(isMaxHeap(heap['heap'])).toBeTruthy();
       max = heap.delMax();
     }
-  })
+  });
+
+  it('Should heapify', () => {
+    const arr1: number[] = [];
+    const arr2: number[] = [];
+    while(arr1.length < 125_342) {
+      arr1.push(Math.floor(Math.random() * 100));
+    }
+    while(arr2.length < 125_342) {
+      arr2.push(Math.floor(Math.random() * 100));
+    }
+
+    const heap1 = BinaryMaxHeapSet.heapify(arr1);
+    // console.log(heap1.print());
+    expect(isMaxHeap(heap1['heap'])).toBeTruthy();
+
+    const heap2 = BinaryMinHeapSet.heapify(arr2);
+    // console.log(heap2.print());
+    expect(isMinHeap(heap2['heap'])).toBeTruthy();
+  });
+
+  it('Should Merge 1 using class', () => {
+    for (let num = 0; num < 1_000; ++num) {
+      console.log('*** TRY:', num);
+
+      const a: number[] = [];
+      const b: number[] = [];
+
+      const al = Math.random() * 12_348;
+      const bl = Math.random() * 11_348;
+      const mx = Math.random() * (al + bl);
+
+      for (let i = 0; i < al; ++i) {
+        a.push(Math.floor(Math.random() * mx))
+      }
+      for (let i = 0; i < bl; ++i) {
+        b.push(Math.floor(Math.random() * mx))
+      }
+
+      const h1 = BinaryMaxHeapSet.heapify<number>(a);
+      const h2 = BinaryMaxHeapSet.heapify<number>(b);
+
+      const h3 = BinaryMaxHeapSet.merge(h1, h2);
+
+      expect(isMaxHeap(h1['heap'])).toBeTruthy();
+      expect(isMaxHeap(h2['heap'])).toBeTruthy();
+
+      expect(isMaxHeap(h3['heap'])).toBeTruthy();
+    }
+  });
+
+  it('Should Merge 1 - test', () => {
+    for (let num = 0; num < 1; ++num) {
+      console.log('*** TRY:', num);
+
+      // const a: number[] = [
+      //   377,
+      //   376,                                                                                                                                                            377, 
+      //   373,                                                                            376,                                                                            373,                                         373, 
+      //   367,                                    368,                                    371,                                    374,                                    366,                     373,                368,                350,
+      //   360,                358,                322,                349,                341,                353,                373,                371,                358,                350, 361,      367,      362,      352,      336,      344,
+      //   355,      347,      351,      343,      308,      319,      331,      318,      315,      338,      294,      326,      346,      365,      270,      277,      306,      335,      341, 316, 360, 337, 364, 337, 343, 322, 339, 329, 327, 335, 319, 327,
+      //   314, 268, 328, 341, 324, 350, 336, 328, 268, 275, 211, 304, 304, 305, 271, 315, 313, 309, 282, 250, 199, 260, 323, 279, 180, 327, 318, 297, 124, 252, 264, 237, 278, 220, 301, 334, 327
+      // ];
+      // const b: number[] = [
+      //   352, 
+      //   345,                                                                           331, 
+      //   343,                                    345,                                   284,                287,
+      //   264,                328,                265,                333,               230,      230,      202,      246,
+      //   260,       99,      226,      321,      261,      257,      268,     146,      134,  84, 221,  30,  25,  50,  62, 110,
+      //   228, 169,  56,  47,  74, 165, 259,  58, 249, 100,  87, 213, 210, 255, 37, 120,  11
+      // ];
+
+      const a: number[] = [
+        377,
+        376,                                    377, 
+        373,                376,                373,                373, 
+        367,      368,      371,      374,      366,      373,      368,      350,
+        360, 358, 322, 349, 341, 353, 373, 371, 358, 350, 361, 367, 362, 352, 336, 344
+      ];
+      const b: number[] = [
+        352, 
+        345,                331, 
+        343,      345,      284,      287,
+        264, 328, 265, 333, 230, 230, 202, 246
+      ];
+
+      expect(isMaxHeap(a)).toBeTruthy();
+      expect(isMaxHeap(b)).toBeTruthy();
+
+      const h3 = mergeHeaps2(a, b);
+
+      expect(isMaxHeap(h3)).toBeTruthy();
+    }
+  });
+
+  it('Should Merge 2', () => {
+    for (let num = 0; num < 1000; ++num) {
+      const a: number[] = [];
+      const b: number[] = [];
+
+      const al = Math.random() * 1_348;
+      const bl = Math.random() * 1_348;
+      const mx = Math.random() * (al + bl);
+
+      for (let i = 0; i < al; ++i) {
+        a.push(Math.floor(Math.random() * mx))
+      }
+      for (let i = 0; i < bl; ++i) {
+        b.push(Math.floor(Math.random() * mx))
+      }
+
+      const h1 = BinaryMaxHeapSet.heapify<number>(a);
+      const h2 = BinaryMaxHeapSet.heapify<number>(b);
+
+
+      // Function call
+      const res = mergeHeaps3(h1['heap'], h2['heap']);
+
+      expect(isMaxHeap(res)).toBeTruthy();
+    }
+  });
+
+  it('Should Meld 1', () => {
+    for (let num = 0; num < 25; ++num) {
+      const a: number[] = [];
+      const b: number[] = [];
+
+      const al = Math.random() * 12_348;
+      const bl = Math.random() * 12_348;
+      const mx = Math.random() * (al + bl);
+
+      for (let i = 0; i < al; ++i) {
+        a.push(Math.floor(Math.random() * mx))
+      }
+      for (let i = 0; i < bl; ++i) {
+        b.push(Math.floor(Math.random() * mx))
+      }
+
+      const h1 = BinaryMaxHeapSet.heapify<number>(a);
+      const h2 = BinaryMaxHeapSet.heapify<number>(b);
+
+      h1.meld(h2);
+      expect(isMaxHeap(h1['heap'])).toBeTruthy();
+    }
+  });
 });
--- src/trees/heap/binary-heap-set.ts
+++ src/trees/heap/binary-heap-set.ts
@@ -2,12 +2,65 @@
  * Based on Java Algorithms and Clients https://algs4.cs.princeton.edu/code/
  * https://algs4.cs.princeton.edu/code/edu/princeton/cs/algs4/MaxPQ.java.html
  * https://algs4.cs.princeton.edu/code/edu/princeton/cs/algs4/MinPQ.java.html
+ * 
+ * Based on:
+ * https://www.geeksforgeeks.org/building-heap-from-array/
+ * https://www.baeldung.com/cs/merge-two-max-heaps/
  */
+
+type Comparator<K> = (a: K, b: K) => boolean;
 
 class BinaryHeapSet<K> {
   protected heap: K[] = [];
 
-  protected constructor(private comparator: (a: K, b: K) => boolean) {    
+  protected static makeHeap<K>(source: K[], comparator:  Comparator<K>): K[] {
+    const heapify = (arr: K[], i: number): void => {
+      let curr = i;
+      const l = 2 * i + 1;
+      const r = 2 * i + 2;
+  
+      if (l < arr.length && comparator(arr[curr], arr[l])) {
+        curr = l;
+      }
+  
+      if (r < arr.length && comparator(arr[curr], arr[r])) {
+        curr = r;
+      }
+  
+      if (curr !== i) {
+          BinaryHeapSet.exch<K>(arr, i, curr);
+          heapify(arr, curr);
+      }
+    }
+
+    const start = Math.floor(source.length / 2) - 1;
+
+    for (let i = start; i >= 0; --i) {
+      heapify(source, i);
+    }
+
+    return source;
+  }
+
+  protected static mergeHeaps<K>(source1: K[], source2: K[], comparator:  Comparator<K>): K[] {
+    if (source1.length === 0) {
+      return source2.slice(0);
+    }
+    if (source2.length === 0) {
+      return source1.slice(0);
+    }
+
+    // Use stright forward solution. Other has errors
+    return BinaryHeapSet.makeHeap(source1.concat(source2), comparator);
+  }
+
+  private static exch<K>(arr: K[], i: number, j: number): void {
+    const swap = arr[i];
+    arr[i] = arr[j];
+    arr[j] = swap;
+  }
+
+  protected constructor(private comparator:  Comparator<K>) {
   }
 
   get size(): number {
@@ -30,121 +83,144 @@ class BinaryHeapSet<K> {
       return false;
     }
 
-    this.exch(index, this.heap.length - 1);
+    this.heap[index] = this.heap[this.heap.length - 1];
     this.heap.pop();
     this.sink(index);
 
     return true;
   }
 
-  replace(key: K): K | undefined {
+  replaceRoot(key: K): K | undefined {
     const root = this.heap[0];
     this.heap[0] = key;
     this.sink(0);
     return root;
   }
 
-  merge(): void {
-
-  }
-
-  meld(): void {
-
-  }
-
-  update(key: K, newKey: K): boolean {
+  update(key: K, newKeyOrUpdateFunc: K | ((oldKey: K) => K)): boolean {
     const index = this.heap.indexOf(key);
     if (index < 0) {
       return false;
     }
 
-    this.heap[index] = newKey;
+    if (newKeyOrUpdateFunc instanceof Function) {
+      this.heap[index] = newKeyOrUpdateFunc(this.heap[index]);
+    } else {
+      this.heap[index] = newKeyOrUpdateFunc;
+    }
 
-    const parent = (index - 1) / 2;
-    if (parent >= 0 && this.compare(parent, index)) {
+    const parent = Math.floor((index - 1) / 2);
+    if (parent >= 0 && this.compareByIndex(parent, index)) {
       this.swim(index);
     } else {
+      let child = -1;
       const child1 = index * 2 + 1;
       const child2 = index * 2 + 2;
 
-      const minChild = this.compare(child1, child2) ? child1 : child2;
-      if (!this.compare(index, minChild)) {
+      if (child1 < this.heap.length && child2 < this.heap.length) {
+        child = this.compareByIndex(child1, child2) ? child1 : child2;
+      } else {
+        if (child1 < this.heap.length) {
+          child = child1;
+        }
+        if (child2 < this.heap.length) {
+          child = child2;
+        }
+      }
+
+      if (child >= 0 && !this.compareByIndex(index, child)) {
         this.sink(index);
       }
     }
+
+    return true;
   }
 
   has(key: K): boolean {
     return this.heap.includes(key);
   }
 
-  // todo
-  /*
-  increaseKey(key: K, delta: K): boolean {
-    const index = this.heap.indexOf(key);
-    if (index < 0) {
-      return false;
+  print(): string {
+    let size = 1;
+    let index = 0;
+    let pad = 0;
+    let result = '';
+    while (index < this.heap.length) {
+      let line = ''.padEnd(pad, ' ');
+      let printed = 1;
+
+      for (let i = index; i < index + size && i < this.heap.length; ++i) {
+        line += this.heap[i] + (printed % 2 === 0 ? ' / ' : ', ');
+        printed += 1;
+      }
+      line = line.substring(0, line.length - 2);
+      pad = line.length;
+      index += size;
+      size *= 2;
+      result += line + '\n';
     }
 
-    this.heap[index] += delta;
-    this.swim(index);
-
-    return true;
+    return result;
   }
-  */
 
-  // todo
-  /*
-  decreaseKey(key: K, delta: K): boolean {
-    const index = this.heap.indexOf(key);
-    if (index < 0) {
-      return false;
+  /**
+   * Destructive merge
+   * 
+   * @throws TypeError if heaps are not compatible
+   */
+  protected meldHeap(other: BinaryHeapSet<K>): void {
+    if (other.heap.length === 0) {
+      return;
     }
-    this.heap[index] -= delta;
-    this.sink(index);
 
-    return true;
-  }
-  */
+    if (this.heap.length === 0) {
+      this.heap = other.heap;
+      other.heap = [];
+      return;
+    }
 
-  // print(): string {
-  //   let size = 1;
-  //   let index = 0;
-  //   let pad = 0;
-  //   let result = '';
-  //   while (index < this.heap.length) {
-  //     let line = ''.padEnd(pad, ' ');
-  //     let printed = 1;
+    // Just for perfomance
+    if (this.heap.length > other.heap.length) {
+      const heap = this.heap;
+      this.heap = other.heap;
+      other.heap = heap;
+    }
 
-  //     for (let i = index; i < index + size && i < this.heap.length; ++i) {
-  //       line += this.heap[i] + (printed % 2 === 0 ? ' / ' : ', ');
-  //       printed += 1;
-  //     }
-  //     line = line.substring(0, line.length - 2);
-  //     pad = line.length;
-  //     index += size;
-  //     size *= 2;
-  //     result += line + '\n';
-  //   }
+    const len = this.heap.length;
+    for (let i = 0; i < len; ++i) {
+      const top = this.heap[i];
+      const otherTop = other.heap[0];
+      if (this.comparator(top, otherTop)) {
+        this.heap[i] = otherTop;
+        other.heap[0] = top;
+        other.sink(0);
+      }
+    }
 
-  //   return result;
-  // }
+    this.heap.push(...other.heap);
 
-  protected root(): K | undefined {
-    return this.heap[0];
+    for (let i = len, l = this.heap.length; i < l; ++i) {
+      this.swim(i);
+    }
+
+    other.heap = [];
   }
 
   protected delRoot(): K | undefined {
+    if (this.heap.length === 1) {
+      return this.heap.pop();
+    }
+
     const root = this.heap[0];
-    this.exch(0, this.heap.length - 1);
+    this.heap[0] = this.heap[this.heap.length - 1];
     this.heap.pop();
     this.sink(0);
     return root;
   }
   
   private swim(index: number): void {
-    while (index > 0 && this.compare(Math.floor((index - 1) / 2), index)) {
-      this.exch(Math.floor((index - 1) / 2), index);
+    while (index > 0 && this.compareByIndex(Math.floor((index - 1) / 2), index)) {
+      BinaryHeapSet.exch(this.heap, Math.floor((index - 1) / 2), index);
       index = Math.floor((index - 1) / 2);
     }
   }
@@ -153,52 +229,95 @@ class BinaryHeapSet<K> {
     while ((index - 1) * 2 <= this.heap.length) {
       let child = index * 2 + 1;
       // find and use max child to swap
-      if (child < this.heap.length - 1 && this.compare(child, child + 1)) {
+      if (child < this.heap.length - 1 && this.compareByIndex(child, child + 1)) {
         child += 1;
       }
-      if (!this.compare(index, child)) {
+      if (!this.compareByIndex(index, child)) {
         break; 
       }
-      this.exch(index, child);
+
+      BinaryHeapSet.exch(this.heap, index, child);
       index = child;
     }
   }
   
-  private exch(i: number, j: number): void {
-    const swap = this.heap[i];
-    this.heap[i] = this.heap[j];
-    this.heap[j] = swap;
-  }
-
-  private compare(i: number, j: number): boolean {
+  private compareByIndex(i: number, j: number): boolean {
     return this.comparator(this.heap[i], this.heap[j]);
   }
 }
 
 export class BinaryMaxHeapSet<K> extends BinaryHeapSet<K> {
-  constructor() {
-    super((a: K, b: K) => a < b); // Correct!
+  static heapify<K>(source: K[]): BinaryMaxHeapSet<K> {
+    const heap = new BinaryMaxHeapSet<K>();
+    heap.heap = super.makeHeap(source.slice(0), BinaryMaxHeapSet.cmpFunc<K>);
+    return heap;
+  }
+
+  /**
+   * Non-Destructive merge
+   */
+  static merge<K>(heap1: BinaryMaxHeapSet<K>, heap2: BinaryMaxHeapSet<K>): BinaryMaxHeapSet<K> {
+    const heap = new BinaryMaxHeapSet<K>();
+    heap.heap = BinaryHeapSet.mergeHeaps(heap1.heap, heap2.heap, BinaryMaxHeapSet.cmpFunc<K>);
+    return heap;
+  }  
+
+  private static cmpFunc = <K>(a: K, b: K) => a < b;  // Correct comparision!
+
+  constructor(){
+    super(BinaryMaxHeapSet.cmpFunc<K>);
   }
 
   max(): K | undefined {
-    return this.root();
+    return this.heap[0];
   }
 
   delMax(): K | undefined {
     return this.delRoot();
   }
+
+  meld(other: BinaryMaxHeapSet<K>): void {
+    this.meldHeap(other);
+  }
 }
 
 export class BinaryMinHeapSet<K> extends BinaryHeapSet<K> {
+  /**
+   * Source array is changed!!!
+   * 
+   * @param source 
+   * @returns 
+   */
+  static heapify<K>(source: K[]): BinaryMinHeapSet<K> {
+    const heap = new BinaryMinHeapSet<K>();
+    heap.heap = super.makeHeap(source.slice(0), BinaryMinHeapSet.cmpFunc<K>);
+    return heap;
+  }
+
+  /**
+   * Non-Destructive merge
+   */
+  static merge<K>(heap1: BinaryMinHeapSet<K>, heap2: BinaryMinHeapSet<K>): BinaryMinHeapSet<K> {
+    const heap = new BinaryMinHeapSet<K>();
+    heap.heap = BinaryHeapSet.mergeHeaps(heap1.heap, heap2.heap, BinaryMinHeapSet.cmpFunc<K>);
+    return heap;
+  }  
+  
+  private static cmpFunc = <K>(a: K, b: K) => a > b;  // Correct comparision!
+
   constructor() {
-    super((a: K, b: K) => a > b); // Correct!
+    super(BinaryMinHeapSet.cmpFunc<K>);
   }
 
   min(): K | undefined {
-    return this.root();
+    return this.heap[0];
   }
 
   delMin(): K | undefined {
     return this.delRoot();
+  }
+
+  meld(other: BinaryMinHeapSet<K>): void {
+    this.meldHeap(other);
   }
 }
